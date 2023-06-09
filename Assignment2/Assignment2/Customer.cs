db12dd2c35b509cb30975ddbfa133d6bec4e0d18
isomer04 <isomer008@gmail.com>
2023-06-08 21:09:49 -0400

    Added Assignment2

---
+++ Assignment2/Assignment2/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public abstract class Customer
+    {
+        protected string _name;
+        public Customer(string name)
+        {
+            _name = name;
+        }
+
+        protected void ChangeName(string name)
+        {
+            _name = name;
+        }
+        
+        protected string GenerateCustomerId(string phoneNumber)
+        {
+            if (!string.IsNullOrEmpty(phoneNumber))
+            {
+                return phoneNumber;
+            }
+            else
+                return "";
+        }
+  
+        public  abstract  void PayBill(double amount);  
+    }
+}
---
+++ Assignment2/Assignment2/VIPCustomer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Assignment2
+{
+    internal class VIPCustomer : Customer
+    {
+        public string type { get; set; }
+
+        public VIPCustomer() : base("name") { }
+
+        public VIPCustomer(string name) : base(name)
+        {
+            _name = name;
+        }
+
+        public bool takeloan(double amount)
+        {
+            return false;
+        }
+
+        public override void PayBill(double amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
