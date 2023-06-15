af0a95c2931b986d03c4e046bea71ea4138cdf24
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-06-15 12:35:58 -0400

    exam1 is task 2 changed

--- src/Exam1/Task2/Program.cs
+++ src/Exam1/Task2/Program.cs
@@ -1,36 +1,46 @@
 ﻿//In this task, you have to create an object oriented design (Class, method, etc.) of a Chess game. [Score: 50]
 
- static void main(String[] args)
+using System;
+
+class Program
 {
-    ChessPieceModel chessPieceModel1 = new ChessPieceModel();
+    static void Main()
+    {
+        ChessPieceModel chessPieceModel1 = new ChessPieceModel();
 
 
-    chessPieceModel1.Name = "King";
-    chessPieceModel1.BoxMovingNumber = 1;
+        chessPieceModel1.Name = "King";
+        chessPieceModel1.BoxMovingNumber = 1;
 
 
 
-    ChessPieceModel chessPieceModel2 = new ChessPieceModel();
+        ChessPieceModel chessPieceModel2 = new ChessPieceModel();
 
-    chessPieceModel2.Name = "Horse";
-    chessPieceModel2.BoxMovingNumber = 2.5f;
+        chessPieceModel2.Name = "Horse";
+        chessPieceModel2.BoxMovingNumber = 2.5f;
 
-    chessPieceModel1.MoveTo();
-    chessPieceModel2.MoveTo();
+        chessPieceModel1.MoveTo();
+        chessPieceModel2.MoveTo();
+
+        /*
+
+        Console.WriteLine("Write the name of piece");
+        chessPieceModel.Name = Console.ReadLine();
+
+
+        Console.WriteLine("Write how many box the piece can move");
+        chessPieceModel.BoxMovingNumber = toInt((Console.ReadLine());
+
+        */
+
+    }
 }
 
 
 
-/*
-
-Console.WriteLine("Write the name of piece");
-chessPieceModel.Name = Console.ReadLine();
 
 
-Console.WriteLine("Write how many box the piece can move");
-chessPieceModel.BoxMovingNumber = toInt((Console.ReadLine());
 
-*/
 
 
 
@@ -40,9 +50,9 @@ public class ChessPieceModel
     public string Name { get; set; }
     public float BoxMovingNumber { get; set; }
 
-    
 
-    internal void MoveTo()
+
+    public void MoveTo()
     {
         Console.WriteLine("This Piece name is " + Name + ". It can move to " + BoxMovingNumber + " box");
     }
