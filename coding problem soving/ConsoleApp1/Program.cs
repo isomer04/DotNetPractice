bada830ef1c17a98896b61f2f24306e5b54e4911
isomer04 <isomer008@gmail.com>
2023-06-16 23:17:25 -0400

    Added new files

---
+++ coding problem soving/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());  
+
+        for (int i = 0; i < number; i++)
+        {
+            string word = Console.ReadLine();  
+
+            if (word.Length > 10)  
+            {
+                string abbreviation = $"{word[0]}{word.Length - 2}{word[^1]}";  
+                Console.WriteLine(abbreviation);
+            }
+            else
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
