fd98fd42259ac952d09e1d66c7c459bfca09ef02
isomer04 <isomer008@gmail.com>
2023-06-19 00:08:59 -0400

    Finished challenge Bear and Big Brother

---
+++ coding problem soving/ConsoleApp1/Bear and Big Brother/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] weights = Console.ReadLine().Split();
+
+        int a = int.Parse(weights[0]);
+        int b = int.Parse(weights[1]);
+
+        int years = 0;
+        while (a <= b)
+        {
+            a *= 3;
+            b *= 2;
+            years++;
+        }
+
+        Console.WriteLine(years);
+    }
+}
