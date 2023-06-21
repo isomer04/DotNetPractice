308e794956c53d5419668ade6abb791088dc725e
isomer04 <isomer008@gmail.com>
2023-06-21 10:01:34 -0400

    Solved problem drink

---
+++ coding problem soving/ConsoleApp1/Drinks/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int inputDigit1 = int.Parse(Console.ReadLine());
+        string[] input = Console.ReadLine().Split(' ');
+
+
+        double responses = 0;
+
+        for (int i = 0; i < inputDigit1; i++)
+        {
+            responses += int.Parse(input[i]);
+        }
+
+        responses = responses / inputDigit1;
+
+        Console.WriteLine(responses);
+
+    }
+}
