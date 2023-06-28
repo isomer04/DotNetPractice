8052b3567fd7885195ad1a25d72f29e6a98c5623
isomer04 <isomer008@gmail.com>
2023-06-27 23:38:05 -0400

    Solved soft drinking

---
+++ coding problem soving/ConsoleApp1/Soft Drinking/Program.cs
@@ -0,0 +1,29 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] inputArr = Console.ReadLine().Split();
+        
+        int numOfFriends = int.Parse(inputArr[0]);
+        int numberOfBottles = int.Parse(inputArr[1]);
+        int millilitersBottles = int.Parse(inputArr[2]);
+        int numberOfLimes = int.Parse(inputArr[3]);
+        int slicesPerLime = int.Parse(inputArr[4]);
+        int totalSalt = int.Parse(inputArr[5]);
+        int millitersPerToast = int.Parse(inputArr[6]);
+        int gramsOfSaltPerToast = int.Parse(inputArr[7]);
+
+        int totalDrink = numberOfBottles * millilitersBottles;
+        int totalLimes = numberOfLimes * slicesPerLime;
+
+        int maxToasts = Math.Min(totalDrink / (numOfFriends * millitersPerToast),
+            Math.Min(totalLimes / numOfFriends,
+            totalSalt / (numOfFriends * gramsOfSaltPerToast)));
+
+
+        Console.WriteLine(maxToasts);
+    }
+}
+
+
