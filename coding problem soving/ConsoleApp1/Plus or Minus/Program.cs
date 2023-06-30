157de720fdacb8555230843e325b9be510a80098
isomer04 <isomer008@gmail.com>
2023-06-30 17:00:42 -0400

    Solved problem Two elevators

---
+++ coding problem soving/ConsoleApp1/Plus or Minus/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int testCases = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < testCases; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            int c = int.Parse(input[2]);
+
+            if( a + b == c ) Console.WriteLine("+");
+            else Console.WriteLine("-");
+        }
+    }
+}
