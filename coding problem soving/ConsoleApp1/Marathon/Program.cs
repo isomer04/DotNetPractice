bba53937d62bc58f04cf9f777a6faa91eb82af5c
isomer04 <isomer008@gmail.com>
2023-07-01 17:19:02 -0400

    Solved problems Marathon and Triple

---
+++ coding problem soving/ConsoleApp1/Marathon/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int testCases = int.Parse(Console.ReadLine());
+
+        for(int  i = 0; i < testCases; i++)
+        {
+            int participantsFront = 0;
+
+
+            string[] distances = Console.ReadLine().Split();
+            int a  = int.Parse(distances[0]);
+            int b = int.Parse(distances[1]);
+            int c = int.Parse(distances[2]);
+            int d = int.Parse(distances[3]);
+
+            if (b > a || c > a || d > a)
+                participantsFront++;
+            //if (c > a)
+            //    participantsFront++;
+            //if (d > a)
+            //    participantsFront++;
+
+            Console.WriteLine(participantsFront);
+
+        }
+    }
+}
---
+++ coding problem soving/ConsoleApp1/Triple/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int testCases = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < testCases; i++)
+        {
+            int lengthOfElements = int.Parse(Console.ReadLine());
+            int[] elementsArray = new int[lengthOfElements];
+
+            string[] arrayOfElements = Console.ReadLine().Split();
+            for (int j = 0; j < lengthOfElements; j++)
+            {
+                elementsArray[j] = int.Parse(arrayOfElements[j]);
+            }
+
+            Dictionary<int, int> countDict = new Dictionary<int, int>();
+
+            for (int k = 0; k < elementsArray.Length; k++)
+            {
+                if (countDict.ContainsKey(elementsArray[k]))
+                {
+                    countDict[elementsArray[k]]++;
+                }
+                else
+                {
+                    countDict[elementsArray[k]] = 1;
+                }
+
+                if (countDict[elementsArray[k]] >= 3)
+                {
+                    Console.WriteLine(elementsArray[k]);
+                    break;
+                }
+            }
+
+            bool hasValueAppearingThreeTimes = false;
+            foreach (var count in countDict.Values)
+            {
+                if (count >= 3)
+                {
+                    hasValueAppearingThreeTimes = true;
+                    break;
+                }
+            }
+
+            if (!hasValueAppearingThreeTimes)
+            {
+                Console.WriteLine(-1);
+            }
+        }
+    }
+}
