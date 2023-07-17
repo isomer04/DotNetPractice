a35de8b1d80ba2a4d2dd74f085bc35fba9a9a7f4
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-07-17 12:14:03 -0400

    finished task 9

--- Exam2/Task6/Program.cs
+++ Exam2/Task6/Program.cs
@@ -1,6 +1,12 @@
 ﻿//Create a List<> of Tuple. The Tuple should have a string name and int age. Add some value in it and then print all values using loop.
 
 
-(string name, int age) list = ("James wagner", 25);
+using System.Collections.Generic;
 
-Console.WriteLine(list.ToString);
+(string name, int age) lists = ("James wagner", 25);
+
+
+foreach ( list<string, int> in lists)
+{
+    Console.WriteLine(list);
+}
--- Exam2/Task9/Program.cs
+++ Exam2/Task9/Program.cs
@@ -1,1 +1,22 @@
 ﻿//Create a generic method named Reverse that takes a generic parameter. Apply constraint so that this parameter must be of type IList
+
+using System.Collections.Generic;
+
+List<int> nums = new List<int> {1 , 23, 4 };
+
+Reverse(nums);
+
+void Reverse<T>(IList<T> lists)
+{
+
+    //foreach( list in  lists )
+    //{
+    //    Console.WriteLine(list.ToString());
+
+    //}
+
+    for(int i  = 0; i < lists.Count; i++)
+    {
+        Console.WriteLine(lists[i]);
+    }
+}
